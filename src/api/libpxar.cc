cb8528a5f0bc01f19bcd24885b240a39ef38e984
Simon Spannagel <simon.spannagel@desy.de>
2013-12-05 20:34:35 +0100

    Added new interface class pXarAPI.

--- src/api/libpxar.cc
+++ src/api/libpxar.cc
@@ -1,111 +1,52 @@
 #include "libpxar.h"
 #include <iostream>
 
-bool CTestboard::EnumNext(string &name)
-{
-	char s[64];
-	if (!usb.EnumNext(s)) return false;
-	name = s;
-	return true;
+pXarAPI::pXarAPI(std::string name) {
+  board_id = name;
+  tb = new CTestboard();
+
+  //FIXME throw error here, returns bool!
+  tb->FindDTB(board_id);
+
+  if(tb->Open(board_id)) {
+    printf("\nBoard %s opened\n", board_id.c_str());
+    try {
+      PrintInfo();
+      CheckCompatibility();
+      tb->Welcome();
+      tb->Flush();
+      printf("RPC version: %i\n", tb->GetRpcVersion());
+    }
+    catch(CRpcError &e) {
+      e.What();
+      printf("ERROR: DTB software version could not be identified, please update it!\n");
+      tb->Close();
+      printf("Connection to Board %s has been cancelled\n", board_id.c_str());
+    }
+  }
+  else {
+    printf("USB error: %s\n", tb->ConnectionError());
+    printf("DTB: could not open port to device.\n");
+    printf("Make sure you have permission to access USB devices.\n");
+  }
+  tb->Init();
+  
 }
 
-bool CTestboard::Enum(unsigned int pos, string &name)
-{
-	char s[64];
-	if (!usb.Enum(s, pos)) return false;
-	name = s;
-	return true;
+void pXarAPI::Configure() {
+  
 }
 
-
-bool CTestboard::FindDTB(string &usbId)
-{
-	string name;
-	vector<string> devList;
-	unsigned int nDev;
-	unsigned int nr;
-
-	try
-	{
-		if (!EnumFirst(nDev)) throw int(1);
-		for (nr=0; nr<nDev; nr++)
-		{
-			if (!EnumNext(name)) continue;
-			if (name.size() < 4) continue;
-			if (name.compare(0, 4, "DTB_") == 0) devList.push_back(name);
-		}
-	}
-	catch (int e)
-	{
-		switch (e)
-		{
-		case 1: printf("Cannot access the USB driver\n"); return false;
-		default: return false;
-		}
-	}
-
-	if (devList.size() == 0)
-	{
-		printf("No DTB connected.\n");
-		return false;
-	}
-
-	if (devList.size() == 1)
-	{
-		usbId = devList[0];
-		return true;
-	}
-
-	// If more than 1 connected device list them
-	printf("\nConnected DTBs:\n");
-	for (nr=0; nr<devList.size(); nr++)
-	{
-		printf("%2u: %s", nr, devList[nr].c_str());
-		if (Open(devList[nr], false))
-		{
-			try
-			{
-				unsigned int bid = GetBoardId();
-				printf("  BID=%2u\n", bid);
-			}
-			catch (...)
-			{
-				printf("  Not identifiable\n");
-			}
-			Close();
-		}
-		else printf(" - in use\n");
-	}
-
-	printf("Please choose DTB (0-%u): ", (nDev-1));
-	char choice[8];
-	fgets(choice, 8, stdin);
-	sscanf (choice, "%d", &nr);
-	if (nr >= devList.size())
-	{
-		nr = 0;
-		printf("No DTB opened\n");
-		return false;
-	}
-
-	usbId = devList[nr];
-	return true;
+void pXarAPI::PrintInfo() {
+  std::string info;
+  tb->GetInfo(info);
+  printf("--- DTB info-------------------------------------\n"
+	 "%s"
+	 "-------------------------------------------------\n",
+	 info.c_str());
 }
 
-
-bool CTestboard::Open(string &usbId, bool init)
-{
-	rpc_Clear();
-	if (!usb.Open(&(usbId[0]))) return false;
-
-	if (init) Init();
-	return true;
+void pXarAPI::CheckCompatibility() {
+  // Version checks for RPC interface
+  
 }
-
-
-void CTestboard::Close()
-{
-	usb.Close();
-	rpc_Clear();
-}
-
