c978fb92a935f47becbcf1b8b6685e81fd6744c6
Simon Spannagel <simon.spannagel@desy.de>
2013-12-16 09:14:25 +0100

    minor changes, just for testing...

--- main/pxar.cpp
+++ main/pxar.cpp
@@ -7,15 +7,15 @@ int main(int argc, char * argv[])
 
   // Create new API instance:
   try {
-    //    api_inst = new pxar::api();
+    api_inst = new pxar::api();
   }
   catch (...) {
     std::cout << "pXar cauhgt an exception from the board. Exiting." << std::endl;
     return -1;
   }
 
-
-  sleep(10);
+  std::cout << "Done." << std::endl;
+  sleep(2);
 
   delete api_inst;
   return 0;
