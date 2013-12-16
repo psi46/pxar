d21a2df60a7215b1d9d86fc11f4eb2aa7e81dda5
Simon Spannagel <simon.spannagel@desy.de>
2013-12-16 11:56:17 +0100

    Removed FindDTB function from CTestboard class. This should only provide the RPC boilerplate but no actual code. Moved the functionality into the HAL class.

--- core/rpc/rpc_impl.h
+++ core/rpc/rpc_impl.h
@@ -18,8 +18,6 @@ public:
 	}
 	~CTestboard() { RPC_EXIT }
 
-	int test(int a) { return a + 5; }
-
 	int32_t GetHostRpcCallCount() { return rpc_cmdListSize; }
 	bool GetHostRpcCallName(int32_t id, stringR &callName) { callName = rpc_cmdName[id]; return true; }
 
@@ -35,68 +33,6 @@ public:
 	RPC_EXPORT bool    GetRpcCallName(int32_t id, stringR &callName);
 
 	// === DTB connection ====================================================
-
-	bool FindDTB(string &usbId) {
-	  string name;
-	  vector<string> devList;
-	  unsigned int nDev;
-	  unsigned int nr;
-
-	  try {
-	    if (!EnumFirst(nDev)) throw int(1);
-	    for (nr=0; nr<nDev; nr++)	{
-	      if (!EnumNext(name)) continue;
-	      if (name.size() < 4) continue;
-	      if (name.compare(0, 4, "DTB_") == 0) devList.push_back(name);
-	    }
-	  }
-	  catch (int e) {
-	    switch (e) {
-	    case 1: printf("Cannot access the USB driver\n"); return false;
-	    default: return false;
-	    }
-	  }
-
-	  if (devList.size() == 0) {
-	    printf("No DTB connected.\n");
-	    return false;
-	  }
-
-	  if (devList.size() == 1) {
-	    usbId = devList[0];
-	    return true;
-	  }
-
-	  // If more than 1 connected device list them
-	  printf("\nConnected DTBs:\n");
-	  for (nr=0; nr<devList.size(); nr++) {
-	    printf("%2u: %s", nr, devList[nr].c_str());
-	    if (Open(devList[nr], false)) {
-	      try {
-		unsigned int bid = GetBoardId();
-		printf("  BID=%2u\n", bid);
-	      }
-	      catch (...) {
-		printf("  Not identifiable\n");
-	      }
-	      Close();
-	    }
-	    else printf(" - in use\n");
-	  }
-
-	  printf("Please choose DTB (0-%u): ", (nDev-1));
-	  char choice[8];
-	  fgets(choice, 8, stdin);
-	  sscanf (choice, "%ud", &nr);
-	  if (nr >= devList.size()) {
-	    nr = 0;
-	    printf("No DTB opened\n");
-	    return false;
-	  }
-
-	  usbId = devList[nr];
-	  return true;
-	};
 
 	inline bool Open(string &name, bool init=true) {
 	  rpc_Clear();
