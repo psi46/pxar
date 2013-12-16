f656af74b9874b973fb0026a56b009e8df9a6b5f
Simon Spannagel <simon.spannagel@desy.de>
2013-12-16 11:44:52 +0100

    Documentation of the first HAL member functions

--- core/hal/hal.h
+++ core/hal/hal.h
@@ -9,18 +9,45 @@ namespace pxar {
   {
 
   public:
+    /** Default constructor for cerating a new HAL instance. Takes
+     *  a testboard USB ID name as parameter and tries to connect to
+     *  the board. Exception is thrown if connection fails.
+     */
     hal(std::string name = "*");
 
+    /** Default destructor for HAL objects. Testboard USB connection is
+     *  closed and RPC object destroyed.
+     */
+    ~hal();
+
+    /** Configure FIXME
+     */
     void Configure();
 
   private:
 
-    /** Private instance of the testboard RPC interface
+    /** Private instance of the testboard RPC interface, routes all
+     *  hardware access:
      */
     CTestboard * tb;
 
+    /** Print the info block with software and firmware versions,
+     *  MAC and USB ids etc. read from the connected testboard
+     */
     void PrintInfo();
+
+    /** Check for matching pxar / testboard software and firmware versions
+     * and scan the RPC commands one by one if ion doubt.
+     */
     void CheckCompatibility();
+
+    /** Find attached USB devices that match the DTB naming scheme.
+     *
+     *  If usbId = "*" check for all attached devices and list them,
+     *  if a specific USB ID is given check that device and return.
+     */
+    bool FindDTB(std::string &usbId);
+
 
   };
 
