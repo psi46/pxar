f359ca7a59b943f02f2b7cbef8474ae00be72c99
Hanno Perrey <hanno.perrey@desy.de>
2013-12-19 15:23:36 +0100

    added some implementations of DUT class methods

--- core/api/api.cc
+++ core/api/api.cc
@@ -5,6 +5,7 @@
 #include "api.h"
 #include "hal.h"
 #include <iostream>
+#include <algorithm>
 
 using namespace pxar;
 
@@ -37,8 +38,19 @@ bool api::initDUT(std::vector<std::pair<uint8_t,uint8_t> > dacVector) {
   // this is highly incomplete and is only a demonstration for single ROCs
   //for(nrocs)
 
-  rocConfig newroc;
+  rocConfig newroc = {};
   newroc.dacs = dacVector;
+  for (uint8_t column=1;column<=52;column++){
+    for (uint8_t row=1;row<=80;row++){
+      pixelConfig newpix = {};
+      newpix.row = row;
+      newpix.column = column;
+      newpix.enable = true;
+      newpix.mask = false;
+      newpix.trim = column+row; // arbitray but varying values for debugging
+      newroc.pixels.push_back(newpix);
+    }
+  }
   newroc.type = 0x0; //ROC_PSI46DIGV2;
   newroc.enable = true;
   _dut->roc.push_back(newroc);
@@ -139,25 +151,108 @@ bool api::daqStop() {
 
 
 
+/** Helper class to search vectors of pixelConfig, rocConfig and dacConfig for 'enable' bit
+ */
+class configEnableSet
+{
+  const bool _isEnable;
+
+public:
+  configEnableSet(const bool pEnable) : _isEnable(pEnable) {}
+
+  template<class ConfigType>
+  bool operator()(const ConfigType &config) const
+  {
+    return config.enable == _isEnable;
+  }
+};
+
+
+/** Helper class to search vectors of pixel or pixelConfig for 'column' and 'row' values
+ */
+class findPixelXY
+{
+  const uint8_t _column;
+  const uint8_t _row;
+
+public:
+  findPixelXY(const uint8_t pColumn, const uint8_t pRow) : _column(pColumn), _row(pRow) {}
+
+  template<class ConfigType>
+  bool operator()(const ConfigType &config) const
+  {
+    return (config.row == _row) && (config.column == _column);
+  }
+};
+
+
+
 
 /** DUT class functions **/
 
-int32_t dut::getEnabledPixels(size_t rocId) {}
+int32_t dut::getNEnabledPixels() {
+  if (!_initialized) return 0;
+  // search for pixels that have enable set
+  std::vector<pixelConfig>::iterator it = std::find_if(roc.at(0).pixels.begin(),
+						       roc.at(0).pixels.end(),
+						       configEnableSet(true));
+  int32_t count = 0;
+  // loop over result, count pixel
+  while(it != roc.at(0).pixels.end()){
+    count++;
+    it++;
+  }
+  return count;
+}
 
-bool dut::getPixelEnabled(uint8_t column, uint8_t row, size_t rocId) {
+std::vector< pixelConfig > dut::getEnabledPixels(size_t rocid) {
+  std::vector< pixelConfig > result;
+  if (!_initialized) return result;
+  // search for pixels that have enable set
+  std::vector<pixelConfig>::iterator it = std::find_if(roc.at(rocid).pixels.begin(),
+						       roc.at(rocid).pixels.end(),
+						       configEnableSet(true));
+  while(it != roc.at(rocid).pixels.end()){
+    result.push_back(*it);
+    it++;
+  }
+  return result;
+}
+
+
+bool dut::getPixelEnabled(uint8_t column, uint8_t row) {
+  std::vector<pixelConfig>::iterator it = std::find_if(roc.at(0).pixels.begin(),
+						       roc.at(0).pixels.end(),
+						       findPixelXY(column,row));
+  if(it != roc.at(0).pixels.end())
+    return it->enable;
   return false;
 }
+
+pixelConfig dut::getPixelConfig(size_t rocid, uint8_t column, uint8_t row) {
+  pixelConfig result = {}; // init with 0s
+  if (!_initialized) return result;
+  // find pixel with specified column and row
+  std::vector<pixelConfig>::iterator it = std::find_if(roc.at(rocid).pixels.begin(),
+						       roc.at(rocid).pixels.end(),
+						       findPixelXY(column,row));
+  // if pixel found, set result accordingly
+  if(it != roc.at(rocid).pixels.end()){
+    result = *it;
+  }
+  return result;
+}
+
 
 uint8_t dut::getDAC(size_t rocId, std::string dacName) {}
 
 std::vector<std::pair<uint8_t,uint8_t> > dut::getDACs(size_t rocId) {}
 
 void dut::printDACs(size_t rocId) {
-  
   if(rocId < roc.size()) {
       std::cout << "Printing current DAC settings for ROC " << rocId << ":" << std::endl;
-      for(std::vector< std::pair<uint8_t,uint8_t> >::iterator it = roc[rocId].dacs.begin(); 
-	  it != roc[rocId].dacs.end(); ++it) {
+      for(std::vector< std::pair<uint8_t,uint8_t> >::iterator it = roc.at(rocId).dacs.begin(); 
+	  it != roc.at(rocId).dacs.end(); ++it) {
 	std::cout << "DAC" << (int)it->first << " = " << (int)it->second << std::endl;
       }
     }
@@ -179,9 +274,29 @@ void dut::setTBMEnable(size_t tbmId, bool enable) {
     tbm[tbmId].enable = enable;
 }
 
-void dut::setPixelEnable(uint8_t column, uint8_t row, bool enable) {}
+void dut::setPixelEnable(uint8_t column, uint8_t row, bool enable) {
+  if (!_initialized) return;
+  // loop over all rocs (not strictly needed but used for consistency)
+  for (std::vector<rocConfig>::iterator rocit = roc.begin() ; rocit != roc.end(); ++rocit){
+    // find pixel with specified column and row
+    std::vector<pixelConfig>::iterator it = std::find_if(rocit->pixels.begin(),
+							 rocit->pixels.end(),
+							 findPixelXY(column,row));
+    // set enable
+    if(it != rocit->pixels.end())
+      it->enable = enable;
+  }
+}
 
-void dut::setAllPixelEnable(bool enable) {}
+void dut::setAllPixelEnable(bool enable) {
+  // loop over all rocs (not strictly needed but used for consistency)
+  for (std::vector<rocConfig>::iterator rocit = roc.begin() ; rocit != roc.end(); ++rocit){
+    // loop over all pixel, set enable according to parameter
+    for (std::vector<pixelConfig>::iterator pixelit = rocit->pixels.begin() ; pixelit != rocit->pixels.end(); ++pixelit){
+      pixelit->enable = enable;
+    }
+  }
+}
 
 bool dut::status() {
 
