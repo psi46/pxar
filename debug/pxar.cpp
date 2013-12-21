e0f1a31fa3057efe2413bdb82c46339886f5deb7
Simon Spannagel <simonspa@kth.se>
2013-12-21 18:53:05 +0100

    Make testpxar aware of the changed API interface. Now initializing the DUT with the proper command (hopefully the final syntax...)

--- debug/pxar.cpp
+++ debug/pxar.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include "pxar.h"
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -11,38 +12,63 @@ int main()
   
     // Try some test or so:
 
-    // Create some fake DAC parameters since we can't read configs yet:
+    // Create some fake DUT/DAC parameters since we can't read configs yet:
+    std::vector<std::vector<std::pair<std::string,uint8_t> > > rocDACs;
 
-    std::vector<std::pair<uint8_t,uint8_t> > dacs;
-    dacs.push_back(std::make_pair(1,6)); //Vdig
-    dacs.push_back(std::make_pair(2,84)); //Vana
-    dacs.push_back(std::make_pair(3,30)); //Vsf
-    dacs.push_back(std::make_pair(4,12)); //Vcomp
-    dacs.push_back(std::make_pair(7,60)); //VwllPr
-    dacs.push_back(std::make_pair(9,60)); //VwllSh
-    dacs.push_back(std::make_pair(10,230)); //VhldDel
-    dacs.push_back(std::make_pair(11,29)); //Vtrim
-    dacs.push_back(std::make_pair(12,86)); //VthrComp
-    dacs.push_back(std::make_pair(13,1)); //VIBias_Bus
-    dacs.push_back(std::make_pair(14,6)); //Vbias_sf
-    dacs.push_back(std::make_pair(15,40)); //VoffsetOp
-    dacs.push_back(std::make_pair(17,129)); //VOffsetR0
-    dacs.push_back(std::make_pair(18,120)); //VIon
-    dacs.push_back(std::make_pair(19,100)); //Vcomp_ADC
-    dacs.push_back(std::make_pair(20,91)); //VIref_ADC
-    dacs.push_back(std::make_pair(21,150)); //VIbias_roc
-    dacs.push_back(std::make_pair(22,50)); //VIColOr
-    dacs.push_back(std::make_pair(25,200)); //Vcal
-    dacs.push_back(std::make_pair(26,122)); //CalDel
-    dacs.push_back(std::make_pair(253,0)); //CtrlReg
-    dacs.push_back(std::make_pair(254,100)); //WBC
+    std::vector<std::pair<std::string,uint8_t> > dacs;
+    dacs.push_back(std::make_pair("Vdig",6));
+    dacs.push_back(std::make_pair("Vana",84));
+    dacs.push_back(std::make_pair("Vsf",30));
+    dacs.push_back(std::make_pair("Vcomp",12));
+    dacs.push_back(std::make_pair("VwllPr",60));
+    dacs.push_back(std::make_pair("VwllSh",60));
+    dacs.push_back(std::make_pair("VhldDel",230));
+    dacs.push_back(std::make_pair("Vtrim",29));
+    dacs.push_back(std::make_pair("VthrComp",86));
+    dacs.push_back(std::make_pair("VIBias_Bus",1));
+    dacs.push_back(std::make_pair("Vbias_sf",6));
+    dacs.push_back(std::make_pair("VoffsetOp",40));
+    dacs.push_back(std::make_pair("VOffsetR0",129));
+    dacs.push_back(std::make_pair("VIon",120));
+    dacs.push_back(std::make_pair("Vcomp_ADC",100));
+    dacs.push_back(std::make_pair("VIref_ADC",91));
+    dacs.push_back(std::make_pair("VIbias_roc",150));
+    dacs.push_back(std::make_pair("VIColOr",50));
+    dacs.push_back(std::make_pair("Vcal",200));
+    dacs.push_back(std::make_pair("CalDel",122));
+    dacs.push_back(std::make_pair("CtrlReg",0));
+    dacs.push_back(std::make_pair("WBC",100));
     std::cout << (int)dacs.size() << " DACs loaded.\n";
+
+    rocDACs.push_back(dacs);
+
+    // Get some pixelConfigs up and running:
+    std::vector<std::vector<pxar::pixelConfig> > rocPixels;
+    std::vector<pxar::pixelConfig> pixels;
+
+    for(int col = 0; col < 52; col++) {
+      for(int row = 0; row < 80; row++) {
+	pxar::pixelConfig newpix;
+	newpix.column = col;
+	newpix.row = row;
+	//Trim: fill random value, should report fail as soon as we have range checks:
+	newpix.trim = row;
+	newpix.mask = false;
+	newpix.enable = true;
+
+	pixels.push_back(newpix);
+      }
+    }
+    rocPixels.push_back(pixels);
+
+    // Prepare some empty TBM vector:
+    std::vector<std::vector<std::pair<std::string,uint8_t> > > tbmDACs;
 
     // Initialize the testboard:
     _api->initTestboard();
 
     // Initialize the DUT (power it up and stuff):
-    _api->initDUT(dacs);
+    _api->initDUT("",tbmDACs,"psi46digV3",rocDACs,rocPixels);
 
     // Read current:
     std::cout << "Analog current: " << _api->getTBia()*1000 << "mA" << std::endl;
