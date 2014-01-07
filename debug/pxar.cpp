f200e7dbf1e044e5b0474e9ec18b159ce981a5b4
Simon Spannagel <simon.spannagel@desy.de>
2014-01-07 10:34:02 +0100

    Comment out some of the examples in testpxar, add new test: Efficiency Map (commented out)

--- debug/pxar.cpp
+++ debug/pxar.cpp
@@ -25,12 +25,13 @@ int main()
     _api->initTestboard(sig_delays, power_settings, pg_setup);
 
     //_api->flashTB("/tmp/dtb_v0111.flash");
+    //_api->flashTB("/tmp/Pixel/dtb_v01122.flash");
     
     // Create some fake DUT/DAC parameters since we can't read configs yet:
     std::vector<std::vector<std::pair<std::string,uint8_t> > > rocDACs;
 
     std::vector<std::pair<std::string,uint8_t> > dacs;
-    dacs.push_back(std::make_pair("Vdig",60));
+    dacs.push_back(std::make_pair("Vdig",6));
     dacs.push_back(std::make_pair("Vana",84));
     dacs.push_back(std::make_pair("Vsf",30));
     dacs.push_back(std::make_pair("Vcomp",12));
@@ -91,14 +92,19 @@ int main()
     std::cout << "Analog current: " << _api->getTBia()*1000 << "mA" << std::endl;
     std::cout << "Digital current: " << _api->getTBid()*1000 << "mA" << std::endl;
 
+    sleep(1);
+
     // Do some debug readout: Pulseheight of px 3,3 with 5 triggers:
     _api->debug_ph(3,3,15,5);
 
+    sleep(1);
+
+    /*
     // Check if we can access the DUT data structure and enable/disable stuff:
     //_api->_dut->getDAC(0,"Vana");
     
     // Print DACs from ROC 0:
-    _api->_dut->printDACs(0);
+    //_api->_dut->printDACs(0);
 
     // disable pixel(s)
     _api->_dut->setAllPixelEnable(false);
@@ -113,9 +119,12 @@ int main()
     // debug some DUT implementation details
     std::cout << " have " << _api->_dut->getNEnabledPixels() << " pixels set to enabled" << std::endl;
     std::cout << " have " << (int) _api->_dut->getPixelConfig(0,8,8).trim << " as trim value on pixel 8,8" << std::endl;
+    */
 
+    // ##########################################################
     // call a 'demo' (i.e. fake) DAC scan routine
-    std::vector< std::pair<uint8_t, std::vector<pxar::pixel> > > data = _api->getDebugVsDAC("test", 20, 28, 50, 16);
+    /*
+    std::vector< std::pair<uint8_t, std::vector<pxar::pixel> > > data = _api->getDebugVsDAC("vana", 20, 28, 50, 16);
 
     // check out the data we received:
     std::cout << " number of stored (DAC,pixels) pairs in data: " << data.size() << std::endl;
@@ -127,6 +136,38 @@ int main()
 	std::cout << "       pixel " << (int)  pixit->column << ", " << (int)  pixit->row << " has value "<< (int)  pixit->value <<  std::endl;
       }
     }
+    */
+    // ##########################################################
+
+    
+    // ##########################################################
+    // Call the first real test (pixel efficiency map):
+    /*
+    // Enable all pixels first:
+    _api->_dut->setAllPixelEnable(true);
+
+    // Call the test:
+    int nTrig = 10;
+    std::vector< pxar::pixel > mapdata = _api->getEfficiencyMap(0,nTrig);
+    std::cout << "Data size returned: " << mapdata.size() << std::endl;
+
+    std::cout << "ASCII Sensor Efficiency Map:" << std::endl;
+    unsigned int row = 0;
+    for (std::vector< pxar::pixel >::iterator mapit = mapdata.begin(); mapit != mapdata.end(); ++mapit) {
+      
+      //std::cout << "Px " << (int)mapit->column << ", " << (int)mapit->row << " has efficiency " << (int)mapit->value << "/" << nTrig << " = " << (mapit->value/nTrig) << std::endl;
+
+      if((int)mapit->value == nTrig) std::cout << "X";
+      else std::cout << (int)mapit->value;
+
+      row++;
+      if(row >= 80) { 
+	row = 0;
+	std::cout << std::endl;
+      }
+    }
+    */
+    // ##########################################################
     
     // And end that whole thing correcly:
     std::cout << "Done." << std::endl;
