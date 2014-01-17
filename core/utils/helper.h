74e973b31c6d7a7e655d871c7967b6fe0e76348f
Hanno Perrey <hanno.perrey@desy.de>
2014-01-17 14:41:08 +0100

    moved helper classes from dut implementation into own file in utils folder (for reuse in api impl.)

---
+++ core/utils/helper.h
@@ -0,0 +1,59 @@
+/* This file contains helper classes which are used by API and DUT
+   implementations to search API objects for certain properties */
+
+#ifndef PXAR_HELPER_H
+#define PXAR_HELPER_H
+
+/* =========================================================================== */
+
+/** Helper class to search vectors of pixelConfig, rocConfig and tbmConfig for 'enable' bit
+ */
+class configEnableSet
+{
+  const bool _isEnable;
+
+public:
+  configEnableSet(const bool pEnable) : _isEnable(pEnable) {}
+
+  template<class ConfigType>
+  bool operator()(const ConfigType &config) const
+  {
+    return config.enable == _isEnable;
+  }
+};
+
+/** Helper class to search vectors of pixelConfig for 'mask' bit
+ */
+class configMaskSet
+{
+  const bool _isMasked;
+
+public:
+  configMaskSet(const bool pMask) : _isMasked(pMask) {}
+
+  template<class ConfigType>
+  bool operator()(const ConfigType &config) const
+  {
+    return config.mask == _isMasked;
+  }
+};
+
+/** Helper class to search vectors of pixel or pixelConfig for 'column' and 'row' values
+ */
+class findPixelXY
+{
+  const uint8_t _column;
+  const uint8_t _row;
+
+public:
+  findPixelXY(const uint8_t pColumn, const uint8_t pRow) : _column(pColumn), _row(pRow) {}
+
+  template<class ConfigType>
+  bool operator()(const ConfigType &config) const
+  {
+    return (config.row == _row) && (config.column == _column);
+  }
+};
+
+
+#endif
