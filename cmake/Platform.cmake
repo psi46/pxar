2b352e43de7493c20b50b14b46e7eeccb4d7c1a5
Hanno Perrey <hanno.perrey@desy.de>
2014-01-28 13:53:29 +0100

    moved compiler specific flags to separate file; removed libusb dependency check for win32 for now (needs to be tested)

---
+++ cmake/Platform.cmake
@@ -0,0 +1,22 @@
+# Determine platform- and compiler-specific settings
+
+# compiler specific settings
+if (CMAKE_COMPILER_IS_GNUCC)
+   # add some more general preprocessor defines (only for gcc)
+   message(STATUS "Using gcc-specific CXX flags")
+   SET(GCC_COMPILE_FLAGS "-Wall -Wextra -g -Wno-deprecated -pedantic -Wno-long-long")
+   SET(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} ${GCC_COMPILE_FLAGS}" )
+   SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -fno-inline -fdiagnostics-show-option -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wsign-promo -Wstrict-null-sentinel -Wswitch-default -Wundef" CACHE STRING "Debug options." FORCE )
+   SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall"  CACHE STRING "Relwithdebinfo options." FORCE )
+elseif(if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"))
+   message(STATUS "Using Clang-specific CXX flags")
+   SET(GCC_COMPILE_FLAGS "-Wall -Wextra -g -Wno-deprecated -pedantic -Wno-long-long")
+   SET(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} ${GCC_COMPILE_FLAGS}" )
+   SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -fno-inline -fdiagnostics-show-option -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wsign-promo -Wstrict-null-sentinel -Wswitch-default -Wundef" CACHE STRING "Debug options." FORCE )
+   SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall"  CACHE STRING "Relwithdebinfo options." FORCE )
+elseif(MSVC)
+   message(STATUS "Using MSVC-specific preprocessor identifiers and options")
+   add_definitions("/wd4251") # disables warning concerning dll-interface (comes up for std classes too often)
+   add_definitions("/wd4996") # this compiler warnung is about that functions like fopen are unsafe.
+else()
+endif()
