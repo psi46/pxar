a907b135bacceb371e8af9c4e8ea98bf8bc169ce
Hanno Perrey <hanno.perrey@desy.de>
2014-01-31 18:57:49 +0100

    removed temp files used for experimentation

--- core/cython/type_problem.tmp/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(ext_modules = cythonize(
-           "rect.pyx",                 # our Cython source
-           sources=["Rectangle.cpp"],  # additional source file(s)
-           language="c++",             # generate C++ code
-      ))
--- core/cython/type_problem.tmp/test.py
+++
@@ -1,5 +0,0 @@
-import rect
-from rect import PyRectangle
-
-r = PyRectangle(0,0,2,2)
-print "Length is " + str(r.getLength()) + " and area is " + str(r.getArea())
