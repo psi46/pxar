ace3c6b71edc82e27ded79ee1eb5595561f19495
Simon Spannagel <simon.spannagel@desy.de>
2014-01-31 15:22:03 +0100

    First attempt to implement data pipes.

---
+++ core/hal/datapipe.h
@@ -0,0 +1,76 @@
+#include <exception>
+
+namespace pxar {
+
+  class dataPipeException : public std::exception {
+  public:
+  dataPipeException(const char */*message*/) : std::exception() {};
+  };
+    
+  class dpNotConnected : public dataPipeException {
+  public:
+  dpNotConnected() : dataPipeException("Not connected") {};
+  };
+
+  // Data pipe classes
+
+  template <class T> 
+    class dataSource {
+    // The inheritor must define ReadLast and Read:
+    virtual T ReadLast() = 0;
+    virtual T Read() = 0;
+  public:
+    virtual ~dataSource() {}
+    template <class S> friend class dataSink;
+  };
+    
+  // Null source for not connected sinks
+  template <class T>
+    class nullSource : public dataSource<T> {
+  protected:
+    nullSource() {}
+    nullSource(const nullSource&) {}
+    ~nullSource() {}
+    T ReadLast() { throw dpNotConnected(); }
+    T Read()     { return ReadLast();     }
+    template <class TO> friend class dataSink;
+  };
+
+  // Formward declaration of dataPipe class:
+  template <class TI, class TO> class dataPipe;
+
+  template <class T> 
+    class dataSink {
+  protected: 
+    dataSource<T> *src;
+    static nullSource<T> null;
+  public: 
+  dataSink() : src(&null) {}
+    T GetLast() { return src->ReadLast(); }
+    T Get() { return src->Read(); }
+    void GetAll() { while (true) Get(); }
+    template <class TI, class TO> friend void operator >> (dataSource<TI> &, dataSink<TO> &); 
+    template  <class TI, class TO> friend dataSource<TO>& operator >> (dataSource<TI> &in, dataPipe<TI,TO> &out);
+  };
+
+  template <class T>
+    nullSource<T> dataSink<T>::null;
+   
+  // The data pipe:
+  template <class TI, class TO=TI>
+    class dataPipe : public dataSink<TI>, public dataSource<TO> {};
+
+  // Operator to connect source -> sink; source -> datapipe
+  template <class TI, class TO>
+    void operator >> (dataSource<TI> &in, dataSink<TO> &out) {
+    out.src = &in;
+  }
+    
+  // Operator to connect source -> datapipe -> datapipe -> sink
+  template <class TI, class TO>
+    dataSource<TO>& operator >> (dataSource<TI> &in, dataPipe<TI,TO> &out) {
+    out.src = &in;
+    return out;
+  }
+
+}
