915bb2514dbe25735fcae730ce9752b98cbe81cd
Urs Langenegger <urs.langenegger@psi.ch>
2014-02-04 21:15:03 +0100

    removed (references to) tests merged into pretest (PixTestSetVana, PixTestSetCalDel)

--- main/PixTestFactory.cc
+++ main/PixTestFactory.cc
@@ -11,12 +11,11 @@
 #include "PixTestTbm.hh"
 #include "PixTestDacScan.hh"
 #include "PixTestDacDacScan.hh"
+#include "PixTestPhDacScan.hh"
 #include "PixTestTrim.hh"
 #include "PixTestScurves.hh"
 #include "PixTestSetup.hh"
 #include "PixTestPretest.hh"
-#include "PixTestSetVana.hh"
-#include "PixTestSetCalDel.hh"
 #include "PixTestGainCalibration.hh"
 
 using namespace std;
@@ -54,13 +53,12 @@ PixTest* PixTestFactory::createTest(string name, PixSetup *a) {
   if( !name.compare("curvsdac" ) ) return new PixTestCurrentVsDac(a, "CurVsDac" ); 
   if (!name.compare("dacscan")) return new PixTestDacScan(a, "DacScan"); 
   if (!name.compare("dacdacscan")) return new PixTestDacDacScan(a, "DacDacScan"); 
+  if (!name.compare("phdacscan")) return new PixTestPhDacScan(a, "PhDacScan"); 
   if (!name.compare("gaincalibration")) return new PixTestGainCalibration(a, "GainCalibration"); 
   if (!name.compare("pixelalive")) return new PixTestAlive(a, "PixelAlive"); 
   if (!name.compare("pretest")) return new PixTestPretest(a, "Pretest"); 
   if (!name.compare("scurves")) return new PixTestScurves(a, "Scurves"); 
-  if (!name.compare("setcaldel")) return new PixTestSetCalDel(a, "SetCalDel"); 
   if (!name.compare("setup")) return new PixTestSetup(a, "Setup"); 
-  if (!name.compare("setvana")) return new PixTestSetVana(a, "SetVana"); 
   if (!name.compare("tbm")) return new PixTestTbm(a, "Tbm"); 
   if (!name.compare("trim")) return new PixTestTrim(a, "Trim"); 
   return 0; 
