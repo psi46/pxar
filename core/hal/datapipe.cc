b0034767f9b76c8fca0950de2f871614f4b155e7
Simon Spannagel <simon.spannagel@desy.de>
2014-02-06 18:13:25 +0100

    Add Functionality to split and decode both DESER160 and DESER400 data.

--- core/hal/datapipe.cc
+++ core/hal/datapipe.cc
@@ -24,6 +24,8 @@ namespace pxar {
 
     /*
     LOG(logDEBUGHAL) << "----------------";
+    LOG(logDEBUGHAL) << "Channel " << static_cast<int>(channel) << " DESER400: " << (int)tbm_present;
+    LOG(logDEBUGHAL) << "----------------";
     std::stringstream os;
     for (unsigned int i = 0; i < buffer.size(); i++) {
       os << " " << std::setw(4) << std::setfill('0') << std::hex << (unsigned int)(buffer.at(i));
@@ -31,11 +33,56 @@ namespace pxar {
     LOG(logDEBUGHAL) << os.str();
     LOG(logDEBUGHAL) << "----------------";
     */
+
     return lastSample = buffer[pos++];
   }
 
 
-  rawEvent* dtbEventSplitter::Read() {
+  rawEvent* dtbEventSplitter::SplitDeser400() {
+    record.Clear();
+
+    // If last one had event end marker, get a new sample:
+    if (GetLast() & 0x00f0) { Get(); }
+
+    // If new sample does not have start marker keep on reading until we find it:
+    if (!(GetLast() & 0x0080)) {
+      record.SetStartError();
+      while (!(GetLast() & 0x0080)) Get();
+    }
+
+    // Else keep reading and adding samples until we find any marker.
+    do {
+      // If total event size is too big, break:
+      if (record.GetSize() >= 40000) {
+	record.SetOverflow();
+	break;
+      }
+
+      // FIXME Very first event starts with 0xC - which srews up empty event detection here!
+      // If the event start sample is also event end sample, write and quit:
+      //if((GetLast() & 0xf000) == 0xf000) { break; }
+
+      record.Add(GetLast() & 0x00ff);
+    } while ((Get() & 0x00f0) != 0x00f0);
+
+    // Check if the last read sample has event end marker:
+    if (GetLast() & 0x00f0) record.Add(GetLast() & 0x00ff);
+    // Else set event end error:
+    else record.SetEndError();
+
+    /*
+    LOG(logDEBUGHAL) << "-------------------------";
+    std::stringstream os;
+    for (unsigned int i=0; i<record.GetSize(); i++)
+      os << " " << std::setw(4) << std::setfill('0') << std::hex 
+	 << static_cast<uint16_t>(record[i]);
+    LOG(logDEBUGHAL) << os.str();
+*/
+    return &record;
+  }
+
+
+  rawEvent* dtbEventSplitter::SplitDeser160() {
     record.Clear();
 
     // If last one had event end marker, get a new sample:
@@ -67,18 +114,76 @@ namespace pxar {
     // Else set event end error:
     else record.SetEndError();
 
-    /*
+
     LOG(logDEBUGHAL) << "-------------------------";
     std::stringstream os;
     for (unsigned int i=0; i<record.GetSize(); i++)
       os << " " << std::setw(4) << std::setfill('0') << std::hex 
 	 << static_cast<uint16_t>(record[i]);
     LOG(logDEBUGHAL) << os.str();
-    */
+
     return &record;
   }
 
-  event* dtbEventDecoder::Read() {
+
+  event* dtbEventDecoder::DecodeDeser400() {
+
+    roc_event.header = 0;
+    roc_event.trailer = 0;
+    roc_event.pixels.clear();
+
+    rawEvent *sample = Get();
+
+    uint16_t hdr = 0, trl = 0;
+    unsigned int raw = 0;
+
+    // Get the right ROC id, channel 0: 0-7, channel 1: 8-15
+    int16_t roc_n = -1 + GetChannel() * 8; 
+    
+    
+    for (unsigned int i = 0; i < sample->GetSize(); i++) {
+      int d = (*sample)[i] & 0xf;
+      int q = ((*sample)[i]>>4) & 0xf;
+
+      switch (q) {
+      case  0: break;
+
+      case  1: raw = d; break;
+      case  2: raw = (raw<<4) + d; break;
+      case  3: raw = (raw<<4) + d; break;
+      case  4: raw = (raw<<4) + d; break;
+      case  5: raw = (raw<<4) + d; break;
+      case  6: raw = (raw<<4) + d;
+	{
+	pixel pix(raw);
+	pix.roc_id = roc_n;
+	roc_event.pixels.push_back(pix);
+	break;
+	}
+      case  7: roc_n++; break;
+
+      case  8: hdr = d; break;
+      case  9: hdr = (hdr<<4) + d; break;
+      case 10: hdr = (hdr<<4) + d; break;
+      case 11: hdr = (hdr<<4) + d;
+	roc_event.header = hdr;
+	roc_n = -1 + GetChannel() * 8;
+	break;
+
+      case 12: trl = d; break;
+      case 13: trl = (trl<<4) + d; break;
+      case 14: trl = (trl<<4) + d; break;
+      case 15: trl = (trl<<4) + d;
+	roc_event.trailer = trl;
+	break;
+      }
+    }
+
+    // LOG(logDEBUGHAL) << roc_event;
+    return &roc_event;
+  }
+
+  event* dtbEventDecoder::DecodeDeser160() {
     rawEvent *sample = Get();
     roc_event.header = 0;
     roc_event.pixels.clear();
@@ -96,8 +201,6 @@ namespace pxar {
     }
 
     //LOG(logDEBUGHAL) << roc_event;
-
     return &roc_event;
   }
-
 }
