840ced2502c01858e68a980bd60e16d3ddf93df6
Hanno Perrey <hanno.perrey@desy.de>
2014-02-07 02:06:19 +0100

    added all methods from api, started with implementation (partial dummy content only)

--- core/cython/PyPxarCore.pxd
+++ core/cython/PyPxarCore.pxd
@@ -1,6 +1,7 @@
 # distutils: language = c++
 from libc.stdint cimport uint8_t, int8_t, uint16_t, int16_t, int32_t, uint32_t
 from libcpp.vector cimport vector
+from libcpp.map cimport map
 from libcpp.pair cimport pair
 from libcpp.string cimport string
 from libcpp cimport bool
@@ -26,6 +27,21 @@ cdef extern from "api.h" namespace "pxar":
         pixelConfig(uint8_t column, uint8_t row, uint8_t trim)
 
 cdef extern from "api.h" namespace "pxar":
+    cdef cppclass rocConfig:
+        vector[pixelConfig] pixels
+        map[uint8_t, uint8_t] dacs
+        uint8_t type
+        bool enable
+        rocConfig()
+
+cdef extern from "api.h" namespace "pxar":
+    cdef cppclass tbmConfig:
+        map[uint8_t, uint8_t] dacs
+        uint8_t type
+        bool enable
+        tbmConfig()
+
+cdef extern from "api.h" namespace "pxar":
     cdef cppclass pxarCore:
         pxarCore(string usbId, string logLevel) except +
         string getVersion()
@@ -37,3 +53,39 @@ cdef extern from "api.h" namespace "pxar":
                      string roctype,
                      vector[vector[pair[string,uint8_t]]] rocDACs,
                      vector[vector[pixelConfig]] rocPixels) except +
+
+        bool programDUT()
+        bool status()
+        bool flashTB(string filename)
+        double getTBia()
+        double getTBva()
+        double getTBid()
+        double getTBvd()
+        void HVoff()
+        void HVon()
+        void Poff()
+        void Pon()
+        bool SignalProbe(string probe, string name)
+        bool setDAC(string dacName, uint8_t dacValue, uint8_t rocid)
+        bool setDAC(string dacName, uint8_t dacValue)
+        uint8_t getDACRange(string dacName)
+        bool setTbmReg(string regName, uint8_t regValue, uint8_t tbmid)
+        bool setTbmReg(string regName, uint8_t regValue)
+        vector[pair[uint8_t, vector[pixel]]] getPulseheightVsDAC(string dacName, uint8_t dacMin, uint8_t dacMax, uint16_t flags, uint32_t nTriggers) 
+        vector[pair[uint8_t, vector[pixel]]] getEfficiencyVsDAC(string dacName, uint8_t dacMin, uint8_t dacMax, uint16_t flags, uint32_t nTriggers)
+        vector[pair[uint8_t, vector[pixel]]] getThresholdVsDAC(string dacName, uint8_t dacMin, uint8_t dacMax, uint16_t flags, uint32_t nTriggers)
+        vector[pair[uint8_t, pair[uint8_t, vector[pixel]]]] getPulseheightVsDACDAC(string dac1name, uint8_t dac1min, uint8_t dac1max, string dac2name, uint8_t dac2min, uint8_t dac2max, uint16_t flags, uint32_t nTriggers)
+        vector[pair[uint8_t, pair[uint8_t, vector[pixel]]]] getEfficiencyVsDACDAC(string dac1name, uint8_t dac1min, uint8_t dac1max, string dac2name, uint8_t dac2min, uint8_t dac2max, uint16_t flags, uint32_t nTriggers)
+        vector[pair[uint8_t, pair[uint8_t, vector[pixel]]]] getThresholdVsDACDAC(string dac1name, uint8_t dac1min, uint8_t dac1max, string dac2name, uint8_t dac2min, uint8_t dac2max, uint16_t flags, uint32_t nTriggers)
+        vector[pixel] getPulseheightMap(uint16_t flags, uint32_t nTriggers)
+        vector[pixel] getEfficiencyMap(uint16_t flags, uint32_t nTriggers)
+        vector[pixel] getThresholdMap(string dacName, uint16_t flags, uint32_t nTriggers)
+        int32_t getReadbackValue(string parameterName)
+        bool daqStart(vector[pair[uint16_t, uint8_t]] pg_setup)
+        bool daqStatus()
+        void daqTrigger(uint32_t nTrig)
+        void daqTriggerLoop(uint16_t period)
+        vector[uint16_t] daqGetBuffer()
+        vector[pixel] daqGetEvent()
+        bool daqStop()
+
--- core/cython/PyPxarCore.pyx
+++ core/cython/PyPxarCore.pyx
@@ -4,6 +4,7 @@ from libc.stdint cimport uint8_t, int8_t, uint16_t, int16_t, int32_t, uint32_t
 from libcpp.string cimport string
 from libcpp.pair cimport pair
 from libcpp.vector cimport vector
+from libcpp.map cimport map
 
 cimport PyPxarCore
 
@@ -41,8 +42,9 @@ cdef class PixelConfig:
             self.thisptr = new pixelConfig()
     def __dealloc__(self):
         del self.thisptr
-    cdef to_cobject(self, PixelConfig arg):
-        self.thisptr = arg.thisptr
+    cdef c_clone(self, pixelConfig* p):
+        del self.thisptr
+        thisptr = p
     property column:
         def __get__(self): return self.thisptr.column
         def __set__(self, column): self.thisptr.column = column
@@ -58,6 +60,42 @@ cdef class PixelConfig:
     property mask:
         def __get__(self): return self.thisptr.mask
         def __set__(self, mask): self.thisptr.mask = mask
+
+
+cdef class RocConfig:
+    cdef rocConfig *thisptr      # hold a C++ instance which we're wrapping
+    def __cinit__(self):
+            self.thisptr = new rocConfig()
+    def __dealloc__(self):
+        del self.thisptr
+    property column:
+        def __get__(self): 
+            r = list()
+            for p in self.thisptr.pixels:
+                P = PixelConfig()
+                P.c_clone(&p)
+                r.append(P)
+            return r
+        def __set__(self, value): 
+            cdef vector[pixelConfig] v
+            cdef PixelConfig pc
+            for pc in value:
+                v.push_back( <pixelConfig> pc.thisptr[0])
+            self.thisptr.pixels = v
+    property dacs:
+        def __get__(self): return self.thisptr.dacs
+        def __set__(self, value):
+            cdef map[uint8_t, uint8_t] m
+            for key in value.iterkeys():
+                m[key] = value[key]
+            self.thisptr.dacs = m
+    property type:
+        def __get__(self): return self.thisptr.type
+        def __set__(self, value): self.thisptr.type = value
+    property enable:
+        def __get__(self): return self.thisptr.enable
+        def __set__(self, enable): self.thisptr.enable = enable
+
     
 
 cdef class PyPxarCore:
@@ -113,4 +151,63 @@ cdef class PyPxarCore:
         return self.thisptr.initDUT(tbmtype, td, roctype,rd,rpcs)
     def getVersion(self):
         return self.thisptr.getVersion()
-    
+
+    def programDUT(self):
+        return self.thisptr.programDUT()
+    def status(self):
+        return self.thisptr.status()
+    def flashTB(self, string filename):
+        return self.thisptr.flashTB(filename)
+    def getTBia(self):
+        return self.thisptr.getTBia()
+    def getTBva(self):
+        return self.thisptr.getTBva()
+    def getTBid(self):
+        return self.thisptr.getTBid()
+    def getTBvd(self):
+        return self.thisptr.getTBvd()
+    def HVoff(self):
+        self.thisptr.HVoff()
+    def HVon(self):
+        self.thisptr.HVon()
+    def Poff(self):
+        self.thisptr.Poff()
+    def Pon(self):
+        self.thisptr.Pon()
+    def SignalProbe(self, string probe, string name):
+        return self.thisptr.SignalProbe(probe, name)
+    def setDAC(self, string dacName, uint8_t dacValue, rocid = None):
+        if rocid is None:
+            return self.thisptr.setDAC(dacName, dacValue)
+        else:
+            return self.thisptr.setDAC(dacName, dacValue, rocid)
+    def getDACRange(self, string dacName):
+        return self.thisptr.getDACRange(dacName)
+    def setTbmReg(self, string regName, uint8_t regValue, tbmid = None):
+        if tbmid is None:
+            return self.thisptr.setTbmReg(regName, regValue)
+        else:
+            return self.thisptr.setTbmReg(regName, regValue, tbmid)
+    def getPulseheightVsDAC(self, string dacName, int dacMin, int dacMax, int flags = 0, int nTriggers = 16):
+        cdef vector[pair[uint8_t, vector[pixel]]] r
+        r = self.thisptr.getPulseheightVsDAC(dacName, dacMin, dacMax, flags, nTriggers)
+        for d in range(r.size()):
+            print "test"
+        return "test"
+#    def vector[pair[uint8_t, vector[pixel]]] getEfficiencyVsDAC(self, string dacName, uint8_t dacMin, uint8_t dacMax, uint16_t flags = 0, uint32_t nTriggers=16):
+#    def vector[pair[uint8_t, vector[pixel]]] getThresholdVsDAC(self, string dacName, uint8_t dacMin, uint8_t dacMax, uint16_t flags = 0, uint32_t nTriggers=16):
+#    def vector[pair[uint8_t, pair[uint8_t, vector[pixel]]]] getPulseheightVsDACDAC(self, string dac1name, uint8_t dac1min, uint8_t dac1max, string dac2name, uint8_t dac2min, uint8_t dac2max, uint16_t flags = 0, uint32_t nTriggers=16):
+#    def vector[pair[uint8_t, pair[uint8_t, vector[pixel]]]] getEfficiencyVsDACDAC(self, string dac1name, uint8_t dac1min, uint8_t dac1max, string dac2name, uint8_t dac2min, uint8_t dac2max, uint16_t flags = 0, uint32_t nTriggers=16):
+#    def vector[pair[uint8_t, pair[uint8_t, vector[pixel]]]] getThresholdVsDACDAC(self, string dac1name, uint8_t dac1min, uint8_t dac1max, string dac2name, uint8_t dac2min, uint8_t dac2max, uint16_t flags = 0, uint32_t nTriggers=16):
+#    def vector[pixel] getPulseheightMap(self, uint16_t flags, uint32_t nTriggers):
+#    def vector[pixel] getEfficiencyMap(self, uint16_t flags, uint32_t nTriggers):
+#    def vector[pixel] getThresholdMap(self, string dacName, uint16_t flags, uint32_t nTriggers):
+#    def int32_t getReadbackValue(self, string parameterName):
+#    def bool daqStart(self, vector[pair[uint16_t, uint8_t]] pg_setup):
+#    def bool daqStatus(self):
+#    def void daqTrigger(self, uint32_t nTrig):
+#    def void daqTriggerLoop(self, uint16_t period):
+#    def vector[uint16_t] daqGetBuffer(self):
+#    def vector[pixel] daqGetEvent(self):
+#    def bool daqStop(self):
+
