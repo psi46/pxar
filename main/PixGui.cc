24c41b30f91640d0d43ed311b1c541e6f4100f09
Urs Langenegger <urs.langenegger@psi.ch>
2014-02-14 11:12:24 +0100

    make sure all tests know about id2idx mapping

--- main/PixGui.cc
+++ main/PixGui.cc
@@ -202,6 +202,7 @@ TGMainFrame(p, 1, 1, kVerticalFrame), fWidth(w), fHeight(h) {
     createTab(tests[i].c_str()); 
   }
   fcmbTests->Select(0);
+  fParTab->updateSelection(); // ensure that fId2Idx for all tests is initialized
 
   fH1->AddFrame(h1v1, new TGLayoutHints(kLHintsLeft | kLHintsExpandX | kLHintsExpandY, fBorderN, fBorderN, fBorderN, fBorderN));
   fH1->AddFrame(h1v2, new TGLayoutHints(kLHintsCenterX , fBorderN, fBorderN, fBorderN, fBorderN));
