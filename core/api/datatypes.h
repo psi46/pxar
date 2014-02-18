79c78cbb59e0f987248f1f2ee9fde77a56f9998a
Hanno Perrey <hanno.perrey@desy.de>
2014-02-18 16:31:59 +0100

    log if pixel cannot be correctly decoded
    -added log message (level critical) if the pixel cannot be decoded within NUMCOL and NUMROW limits
    -(hopefully) helps to debug issue #64
    -factorized datatypes into header and impl. files

--- core/api/datatypes.h
+++ core/api/datatypes.h
@@ -50,23 +50,7 @@ namespace pxar {
       column = (address>>8)&63;
       row = (address)&127;
     };
-    // FIXME does not work with inverted address yet!
-    inline void decodeRaw(uint32_t raw, bool invert) {
-      value = (raw & 0x0f) + ((raw >> 1) & 0xf0);
-      int c =    (raw >> 21) & 7;
-      c = c*6 + ((raw >> 18) & 7);
-
-      int r2 =    (raw >> 15) & 7;
-      if(invert) { r2 ^= 0x7; }
-      int r1 = (raw >> 12) & 7;
-      if(invert) { r1 ^= 0x7; }
-      int r0 = (raw >>  9) & 7;
-      if(invert) { r0 ^= 0x7; }
-      int r = r2*36 + r1*6 + r0;
-
-      row = 80 - r/2;
-      column = 2*c + (r&1);
-    };
+    void decodeRaw(uint32_t raw, bool invert);
     uint8_t roc_id;
     uint8_t column;
     uint8_t row;
