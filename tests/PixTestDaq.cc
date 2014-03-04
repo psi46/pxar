cb67744c61982904e1228bde88e2ca7934f75ca2
Robert Stringer <robert.stringer@ku.edu>
2014-03-04 16:08:41 -0600

    Added histogram functions for daqTest.

--- tests/PixTestDaq.cc
+++ tests/PixTestDaq.cc
@@ -87,6 +87,7 @@ void PixTestDaq::doTest() {
 
   PixTest::update(); 
   fDirectory->cd();
+  vector<TH2D*> hits;
 
 
   // All on!
@@ -116,11 +117,45 @@ void PixTestDaq::doTest() {
   vector<pxar::Event> daqdat = fApi->daqGetEventBuffer();
 
   cout << "Number of events read from board: " << daqdat.size() << endl;
-  
+
+  string name("Hits");
+  vector<uint8_t> rocIds = fApi->_dut->getEnabledRocIDs();
+  TH2D *h2(0);
+
+  for (unsigned int iroc = 0; iroc < rocIds.size(); ++iroc){
+    id2idx.insert(make_pair(rocIds[iroc], iroc));
+    h2 = bookTH2D(Form("%s_C%d", name.c_str(), iroc), Form("%s_C%d", name.c_str(), rocIds[iroc]), 52, 0., 52., 80, 0., 80.);
+    h2->SetMinimum(0.);
+    h2->SetDirectory(fDirectory);
+    setTitles(h2, "col", "row");
+    hits.push_back(h2);
+  }
+
+ 
   for(std::vector<pxar::Event>::iterator it = daqdat.begin(); it != daqdat.end(); ++it) {
-    cout << (*it) << std::endl;
+    LOG(logDEBUG) << (*it) << std::endl;
+
+   
+    hits[id2idx[it->roc_id]]->SetBinContent(it->column,it->row,it->value);
+ 
   }
   // We should store the data, probably...
+
+
+  for (unsigned int i = 0; i < hits.size(); ++i) {
+    fHistOptions.insert(make_pair(hits[i], "colz"));
+  }
+
+  copy(hitss.begin(), hits.end(), back_inserter(fHistList));
+
+
+  TH2D *h = (TH2D*)(*fHistList.begin());
+
+  h->Draw(getHistOption(h).c_str());
+  fDisplayedHist = find(fHistList.begin(), fHistList.end(), h);
+
+  PixTest::update();
+ 
 
   LOG(logINFO) << "PixTestDaq::doTest() done";
 }
