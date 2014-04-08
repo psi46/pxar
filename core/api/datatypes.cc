29bea274680a9fa72e306ccc08104fc537c970eb
Wolfram Erdmann <wolfram.erdmann@psi.ch>
2014-04-08 15:02:47 +0200

    verify 0 bit between puls-height nibbles

--- core/api/datatypes.cc
+++ core/api/datatypes.cc
@@ -7,6 +7,9 @@ namespace pxar {
 
   void pixel::decodeRaw(uint32_t raw, bool invert) {
     value = (raw & 0x0f) + ((raw >> 1) & 0xf0);
+    if( (raw & 0x10) >0){
+       LOG(logCRITICAL) << "invalid pulse-height stuff bit from raw value of "<< std::hex << raw << std::dec << ": " << *this;
+    }
     int c =    (raw >> 21) & 7;
     c = c*6 + ((raw >> 18) & 7);
     
