psi46/pxar

commit ef54830b9d5839f0f13f3d744a4f947e1fdfd556

stdint.h is a C99 extension which is not compatible with using CINT on Windows. Make sure that stdint.h is not included for the ROOTCINT step on Windows and insert the appropriate type definitions instead

util/ConfigParameters.hh
@@ -3,13 +3,20 @@
 #ifndef CONFIGPARAMETERS
 #define CONFIGPARAMETERS
 
+/** Cannot use stdint.h when running rootcint on WIN32 */
+#if ((defined WIN32) && (defined __CINT__))
+typedef unsigned short int uint16_t;
+typedef unsigned char uint8_t;
+#else
+#include <stdint.h>
+#endif
+
 #ifdef __CINT__
 #undef __GNUC__
 typedef char __signed; 
 typedef char int8_t; 
 #endif
 
-#include <stdint.h>
 #include <string>
 #include <vector>
 

core/api/api.h
@@ -13,10 +13,19 @@
 #define DLLEXPORT
 #endif
 
+/** Cannot use stdint.h when running rootcint on WIN32 */
+#if ((defined WIN32) && (defined __CINT__))
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef unsigned short int uint16_t;
+typedef unsigned char uint8_t;
+#else
+#include <stdint.h>
+#endif
+
 #include <string>
 #include <vector>
 #include <map>
-#include <stdint.h>
 #include "datatypes.h"
 
 // PXAR Flags

core/api/datatypes.h
@@ -1,7 +1,16 @@
 #ifndef PXAR_TYPES_H
 #define PXAR_TYPES_H
 
+/** Cannot use stdint.h when running rootcint on WIN32 */
+#if ((defined WIN32) && (defined __CINT__))
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef unsigned short int uint16_t;
+typedef unsigned char uint8_t;
+#else
 #include <stdint.h>
+#endif
+
 #include <iostream>
 #include <vector>
 #include <map>

commit 9d1a2e1b453a40847859f4b8b90882b23abf36fb

fix stdint.h parsing for ROOT dict generation on OSX10.9
- addresses issue #98
- reintroduces fix of commit e9291728c21b6d1b3edf350a20e881df859c45d7 affected by regression introduced in ef54830b
- only tested on OSX10.8, awaiting confirmation of fix for 10.9

util/ConfigParameters.hh
@@ -11,12 +11,13 @@
 typedef unsigned short int uint16_t;
 typedef unsigned char uint8_t;
 #else
-#include <stdint.h>
-#endif
 
 #ifdef __CINT__
 #undef __GNUC__
 typedef char int8_t; 
+#endif
+
+#include <stdint.h>
 #endif
 
 #include <string>