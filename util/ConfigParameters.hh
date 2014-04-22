psi46/pxar

commit 9d1a2e1b453a40847859f4b8b90882b23abf36fb

fix stdint.h parsing for ROOT dict generation on OSX10.9
- addresses issue #98
- reintroduces fix of commit e9291728c21b6d1b3edf350a20e881df859c45d7 affected by regression introduced in ef54830b
- only tested on OSX10.8, awaiting confirmation of fix for 10.9

util/ConfigParameters.hh
@@ -11,12 +11,13 @@
 typedef unsigned short int uint16_t;
 typedef unsigned char uint8_t;
 #else
-#include <stdint.h>
-#endif
 
 #ifdef __CINT__
 #undef __GNUC__
 typedef char int8_t; 
+#endif
+
+#include <stdint.h>
 #endif
 
 #include <string>

commit efd0646299562e5d4dbc0ce38a02a6f08e1898f1

addresses build issue on WIN32 due to failing ROOT dict generation
-addresses issue psi46/pxar#105
-fixes regression introduced in 9d1a2e1b453a40847859f4b8b90882b23abf36fb

util/ConfigParameters.hh
@@ -7,6 +7,8 @@
 #if ((defined WIN32) && (defined __CINT__))
 typedef unsigned short int uint16_t;
 typedef unsigned char uint8_t;
+#undef __GNUC__
+typedef char int8_t; 
 #else
 
 #ifdef __CINT__