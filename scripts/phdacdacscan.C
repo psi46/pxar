b848af47dbccbd80c3086e2d40c25e86b89d58ad
Urs Langenegger <urs.langenegger@psi.ch>
2014-04-28 17:22:29 +0200

    script to produce all 2D PH scans

--- scripts/phdacdacscan.C
+++ scripts/phdacdacscan.C
@@ -1,18 +1,15 @@
-// -- Invocation:
-// --------------
-//    ../bin/pXar -c '../scripts/phdacdacscan.C("testROC")'
-//    root ../scripts/phdacdacscan.C(
+// -- Usage:
+// ---------
+//    ../bin/pXar -c '../scripts/phdacdacscan.C("vcal", "phdacdacscan.root", "testROC")'
 
 
 // ----------------------------------------------------------------------
-void phdacdacscan(string cfgdirectory = "testROC") {
+void phdacdacscan(string dac1 = "vcal", string rootfile = "phdacdacscan.root", string cfgdirectory = "testROC") {
   gSystem->Load("../lib/libpxar.dylib");
   gSystem->Load("../lib/libpxargui.dylib");
   gSystem->Load("../lib/libpxartests.dylib");
   gSystem->Load("../lib/libpxarutil.dylib");
   gSystem->Load("../lib/libpxarana.dylib");
-
-  string rootfile = "phdacdacscan.root";
 
   ConfigParameters *configParameters = ConfigParameters::Singleton();
   
@@ -33,11 +30,10 @@ void phdacdacscan(string cfgdirectory = "testROC") {
   PixTest *pt = factory->createTest("DacDacScan", ap); 
   pt->setDAC("ctrlreg", 4); 
   pt->setParameter("PHmap", "1"); 
-  pt->setParameter("DAC1", "vcal"); 
+  pt->setParameter("DAC1", dac1); 
 
   vector<string> dacs = configParameters->getDacs();
   for (unsigned int idac = 0; idac < dacs.size(); ++idac) {
-    //  for (unsigned int idac = 0; idac < 1; ++idac) {
     cout << "===> dacdacscan for vcal vs. " << dacs[idac] << endl;
     pt->setParameter("DAC2", dacs[idac]); 
     pt->doTest(); 
