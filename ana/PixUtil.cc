a5327314ef5e03829027729ab1011f565c15b548
Urs Langenegger <urs.langenegger@psi.ch>
2014-05-07 21:58:48 +0200

    add better color map (from Philipp? Vittorio?)

--- ana/PixUtil.cc
+++ ana/PixUtil.cc
@@ -1,8 +1,24 @@
 #include "PixUtil.hh"
 
 #include "TMath.h"
+#include "TStyle.h"
+#include "TColor.h"
 
 using namespace std;
+
+// ----------------------------------------------------------------------
+void PixUtil::setPlotStyle() {
+  const Int_t NRGBs = 5;
+  const Int_t NCont = 255;
+
+  Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+  Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+  Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+  Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+  TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+  gStyle->SetNumberContours(NCont);
+}
+
 
 // ----------------------------------------------------------------------
 bool PixUtil::bothAreSpaces(char lhs, char rhs) { 
