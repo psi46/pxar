d69533fac7c1bc080cdb45b633e12c6a12ee9d04
Urs Langenegger <urs.langenegger@psi.ch>
2014-05-27 14:55:39 +0200

    turn on HV

--- main/pXar.cc
+++ main/pXar.cc
@@ -38,6 +38,7 @@ void createBackup(string a, string b);
 int main(int argc, char *argv[]){
   
   LOG(logINFO) << "*** Welcome to pxar ***";
+  gSystem->Exec("git status");
 
   // -- command line arguments
   string dir("."), cmdFile("nada"), rootfile("nada.root"), logfile("nada.log"), 
@@ -185,6 +186,7 @@ int main(int argc, char *argv[]){
     runGui(a, argc, argv); 
   } else if (doRunSingleTest) {
     PixTestFactory *factory = PixTestFactory::instance(); 
+    if (configParameters->getHvOn()) api->HVon(); 
     PixTest *t = factory->createTest(runtest, &a);
     t->doTest();
     delete t; 
@@ -194,32 +196,35 @@ int main(int argc, char *argv[]){
     string input; 
     bool stop(false);
     PixTestFactory *factory = PixTestFactory::instance(); 
-    LOG(logINFO) << "enter restricted command line mode";
+    if (configParameters->getHvOn()) api->HVon(); 
+    LOG(logINFO) << "enter 'restricted' command line mode";
     do {
       LOG(logINFO) << "enter test to run";
       cout << "pxar> "; 
       cin >> input; 
       std::transform(input.begin(), input.end(), input.begin(), ::tolower);
-      LOG(logINFO) << "  running: " << input; 
       
       if (!input.compare("gui"))  runGui(a, argc, argv); 
       if (!input.compare("exit")) stop = true; 
       if (!input.compare("quit")) stop = true; 
       if (!input.compare("q")) stop = true; 
 
+      if (stop) break;
+      LOG(logINFO) << "  running: " << input; 
       PixTest *t = factory->createTest(input, &a);
       if (t) {
 	t->doTest();
 	delete t;
+      } else {
+	LOG(logINFO) << "command ->" << input << "<- not known, ignored";
       }
     } while (!stop);
     
 
   }
-
-  LOG(logINFO) << "closing down 1";
   
   // -- clean exit (however, you should not get here)
+  LOG(logINFO) << "closing down 1";
   rfile->Write(); 
   rfile->Close(); 
   
