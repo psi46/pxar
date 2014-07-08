20ab57610b8af788bfa1de42dc2177a2864295dd
Hanno Perrey <hanno.perrey@desy.de>
2014-07-08 18:22:07 +0200

    shell-helper script for python cmd line utility passes cmd line args now

--- python/test_cmd.sh
+++ python/test_cmd.sh
@@ -3,4 +3,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib
 export PYTHONPATH=$PYTHONPATH:../lib
 
 # Run the cmd script requiring python lib and pxar core lib:
-python test_cmd.py
+python test_cmd.py $*
