df0210e51b5b3b196e1f65aee80d95df2aa2edf4
Simon Spannagel <simon.spannagel@desy.de>
2014-07-11 10:41:46 +0200

    Dummy DTB: factorize Noise Hit and Event Generation.

--- core/hal/dummy_hal.cc
+++ core/hal/dummy_hal.cc
@@ -8,6 +8,38 @@
 
 using namespace pxar;
 
+pxar::pixel getNoiseHit(uint8_t rocid, size_t i, size_t j) {
+
+  // Generate a slightly random pulse height between 80 and 100:
+  uint16_t pulseheight = rand()%20 + 80;
+
+  // We can't pulse the same pixel twice in one trigger:
+  size_t col = rand()%52;
+  while(col == i) col = rand()%52;
+  size_t row = rand()%80;
+  while(row == j) row = rand()%80;
+
+  pixel px = pixel(rocid,col,row,pulseheight);
+  LOG(logDEBUGPIPES) << "Adding noise hit: " << px;
+  return px;
+}
+
+void fillEvent(pxar::Event * evt, uint8_t rocid, size_t col, size_t row, uint32_t flags) {
+
+  // Generate a slightly random pulse height between 90 and 100:
+  uint16_t pulseheight = rand() % 2 + 90;
+
+  // Introduce some address encoding issues:
+  if((flags&FLAG_CHECK_ORDER) != 0 && col == 0 && row == 1) { evt->pixels.push_back(pixel(rocid,col,row+1,pulseheight));} // PX 0,1 answers as PX 0,2
+  else if((flags&FLAG_CHECK_ORDER) != 0 && col == 0 && row == 2) { } // PX 0,2 is dead
+  else if((flags&FLAG_CHECK_ORDER) != 0 && col == 0 && row == 6) { evt->pixels.push_back(pixel(rocid,col,row+1,pulseheight));} // PX 0,6 answers as PX 0,7
+  else { evt->pixels.push_back(pixel(rocid,col,row,pulseheight)); }
+
+  // If the full chip is unmasked, add some noise hits:
+  if((flags&FLAG_FORCE_UNMASKED) != 0 && (rand()%4) == 0) { evt->pixels.push_back(getNoiseHit(rocid,col,row)); }
+
+}
+
 hal::hal(std::string /*name*/) :
   _initialized(false),
   _compatible(false),
