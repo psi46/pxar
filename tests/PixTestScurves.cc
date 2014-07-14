9e1aedc3ef5e736077626b193b6cc2d3b33d513d
Urs Langenegger <urs.langenegger@psi.ch>
2014-07-14 11:40:22 +0200

    corrected s-curve doTest (dac range)

--- tests/PixTestScurves.cc
+++ tests/PixTestScurves.cc
@@ -124,12 +124,12 @@ void PixTestScurves::doTest() {
 
   fParDac = "VthrComp"; 
   fParDacLo = 0; 
-  fParDacHi = 250;
+  fParDacHi = 150;
   scurves();
 
   fParDac = "Vcal"; 
   fParDacLo = 0; 
-  fParDacHi = 250;
+  fParDacHi = 120;
   scurves();
 
 
