f8052c30b2d11466dadeb34c34524933bec99e43
Martino Dall'Osso <martino.dall'osso@cern.ch>
2014-07-24 13:32:11 +0200

    new test: Bare Module Test
    Bare Module Test procedure added!
    Simple procedure with contact check (ia check) - Pretest - PixelAlive -
    BBTest.
    
    Use it only for the Bare Module.
    Use it starting from Single Roc folder.
    Restart pXar after each ROC --> different folder per different ROC.
    Remeber to switch off HV and LV before going to contact.
    
    to be fixed: PON and HV buttons on the gui remain green at the end of
    the test even if they are off.

---
+++ tests/PixTestBareModule.hh
@@ -0,0 +1,30 @@
+#ifndef PIXTESTBAREMODULE_H
+#define PIXTESTBAREMODULE_H
+
+#include "PixTest.hh"
+
+class DLLEXPORT PixTestBareModule : public PixTest {
+
+public:
+	PixTestBareModule(PixSetup *, std::string);
+	PixTestBareModule();
+	virtual ~PixTestBareModule();
+	virtual bool setParameter(std::string parName, std::string sval);
+	void init();
+	void setToolTips();
+	void bookHist(std::string);
+	
+	void runCommand(std::string );
+	void doStdTest(std::string );
+	void doTestRoc(int );
+	void doTest();
+
+private:
+
+	int  fParNSteps;
+	bool fStop;
+
+	ClassDef(PixTestBareModule, 1)
+
+};
+#endif
