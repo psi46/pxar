d6567fd620c8e147138185a78d43936f22fb30b6
Martino Dall'Osso <martino.dall'osso@cern.ch>
2014-10-29 14:59:36 +0100

    PixTestTiming - fabs --> abs
    to avoid error message under windows.
    hope that it is not a problem for others.

--- tests/PixTestTiming.cc
+++ tests/PixTestTiming.cc
@@ -181,7 +181,7 @@ void PixTestTiming::ClkSdaScan() {
             GoodSDA=goodsdalist[0];
           } else {
             sort(goodsdalist.begin(),goodsdalist.end());
-            for (size_t isda=1; isda<goodsdalist.size(); isda++) if (fabs(goodsdalist[isda]-goodsdalist[isda-1])>1) goodsdalist[isda] -= 20;
+            for (size_t isda=1; isda<goodsdalist.size(); isda++) if (abs(goodsdalist[isda]-goodsdalist[isda-1])>1) goodsdalist[isda] -= 20;
             sort(goodsdalist.begin(),goodsdalist.end());
             GoodSDA=goodsdalist[round(goodsdalist.size()/2)];
             if (GoodSDA<0) GoodSDA+=20;
