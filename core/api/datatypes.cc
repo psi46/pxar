9af3a57ebe580437e7ca8512028b5908768af13e
Simon Spannagel <simon.spannagel@desy.de>
2014-11-26 11:06:46 +0100

    Rework pxar::pixel::decodeRaw() to throw more precise exceptions

--- core/api/datatypes.cc
+++ core/api/datatypes.cc
@@ -7,14 +7,14 @@ namespace pxar {
 
 
   void pixel::decodeRaw(uint32_t raw, bool invert) {
+    // Get the pulse height:
     setValue(static_cast<double>((raw & 0x0f) + ((raw >> 1) & 0xf0)));
-    if( (raw & 0x10) >0) {
+    if((raw & 0x10) > 0) {
       LOG(logDEBUGAPI) << "invalid pulse-height fill bit from raw value of "<< std::hex << raw << std::dec << ": " << *this;
-      throw DataDecoderError("Error decoding pixel raw value");
+      throw DataInvalidPulseheightError("Error decoding pixel raw value");
     }
-    int c =    (raw >> 21) & 7;
-    c = c*6 + ((raw >> 18) & 7);
-    
+
+    // Decode the pixel address
     int r2 =    (raw >> 15) & 7;
     if(invert) { r2 ^= 0x7; }
     int r1 = (raw >> 12) & 7;
@@ -22,13 +22,14 @@ namespace pxar {
     int r0 = (raw >>  9) & 7;
     if(invert) { r0 ^= 0x7; }
     int r = r2*36 + r1*6 + r0;
-    
     _row = 80 - r/2;
-    _column = 2*c + (r&1);
-    
-    if (_row >= ROC_NUMROWS || _column >= ROC_NUMCOLS){
-      LOG(logDEBUGAPI) << "invalid pixel from raw value of "<< std::hex << raw << std::dec << ": " << *this;
-      throw DataDecoderError("Error decoding pixel raw value");
+    _column = 2*(((raw >> 21) & 7)*6 + ((raw >> 18) & 7)) + (r&1);
+
+    // Perform range checks:
+    if(_row >= ROC_NUMROWS || _column >= ROC_NUMCOLS) {
+      LOG(logDEBUGAPI) << "Invalid pixel from raw value of "<< std::hex << raw << std::dec << ": " << *this;
+      if(_row == ROC_NUMROWS) throw DataCorruptBufferError("Error decoding pixel raw value");
+      else throw DataInvalidAddressError("Error decoding pixel raw value");
     }
   }
 
