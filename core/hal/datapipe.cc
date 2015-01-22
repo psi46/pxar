68be3c9fe7bd248f6698fa6ea9dd6d50806cffbe
Simon Spannagel <simon.spannagel@desy.de>
2015-01-22 16:18:18 +0100

    Fix to the preliminary SoftTBM event splitter: if event is empty, not only the SoftTBM trailer uses "c" as identifier, but also the DESER160 at ROC header position.

--- core/hal/datapipe.cc
+++ core/hal/datapipe.cc
@@ -111,38 +111,38 @@ namespace pxar {
   rawEvent* dtbEventSplitter::SplitSoftTBM() {*/
    record.Clear();
 
-    // If last one had Event end marker, get a new sample:
-    if (!nextStartDetected) { Get(); }
+   // If last one had Event end marker, get a new sample:
+   if (!nextStartDetected) { Get(); }
 
-    // If new sample does not have start marker keep on reading until we find it:
-    if ((GetLast() & 0xe000) != 0xa000) {
-      record.SetStartError();
-      Get();
-    }
-    record.Add(GetLast());
+   // If new sample does not have start marker keep on reading until we find it:
+   while ((GetLast() & 0xe000) != 0xa000) {
+     record.SetStartError();
+     Get();
+   }
+   Get();
+   //record.Add(GetLast());
 
-    // Else keep reading and adding samples until we find any marker.
-    while ((Get() & 0xe000) != 0xc000) {
-      // Check if it's the weird split softTBM header:
-      //if ((GetLast() & 0xe000) == 0xe000) { Get(); }
+   // Else keep reading and adding samples until we find any marker.
+   while ((Get() & 0xef00) != 0xc000) {
+     // Check if it's the weird split softTBM header:
+     if ((GetLast() & 0xe000) == 0xe000) { Get(); }
  
      // Check if the last read sample has Event end marker:
-      if ((GetLast() & 0xe000) == 0xa000) {
-	record.SetEndError();
-	nextStartDetected = true;
-	return &record;
-      }
+     if ((GetLast() & 0xe000) == 0xa000) {
+       record.SetEndError();
+       nextStartDetected = true;
+       return &record;
+     }
 
-      // If total Event size is too big, break:
-      if (record.GetSize() < 40000) record.Add(GetLast());
-      else record.SetOverflow();
-    }
-    record.Add(GetLast());
+     // If total Event size is too big, break:
+     if (record.GetSize() < 40000) record.Add(GetLast());
+     else record.SetOverflow();
+   }
 
-    LOG(logDEBUGPIPES) << "-------------------------";
-    LOG(logDEBUGPIPES) << listVector(record.data,true);
+   LOG(logDEBUGPIPES) << "-------------------------";
+   LOG(logDEBUGPIPES) << listVector(record.data,true);
 
-    return &record;
+   return &record;
   }
 
   Event* dtbEventDecoder::DecodeDeser400() {
