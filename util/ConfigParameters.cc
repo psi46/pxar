f93fe609ba127801eaa90501d4c6665f1bf3c6fa
micha <micha.reichmann@gmail.com>
2016-03-23 15:28:54 +0100

    hubids vector is now correctly written into the configfile

--- util/ConfigParameters.cc
+++ util/ConfigParameters.cc
@@ -805,7 +805,7 @@ bool ConfigParameters::writeConfigParameterFile() {
   fprintf(file, "nModules %i\n", fnModules);
   fprintf(file, "nRocs %i\n", fnRocs);
   fprintf(file, "nTbms %i\n", fnTbms);
-  fprintf(file, "hubId %i\n", fHubIds.front());
+  fprintf(file, "hubId %s\n", vectorToString(fHubIds).c_str());
   fprintf(file, "tbmEnable %i\n", fTbmEnable);
   fprintf(file, "tbmEmulator %i\n", fTbmEmulator);
   fprintf(file, "hvOn %i\n", fHvOn);
