1325d768884ac183a37289976af9bcd6dc903f18
Urs Langenegger <urslangenegger@gmail.com>
2020-09-03 14:25:09 +0200

    put empty really to 255.

--- tests/PixTestBBMap.cc
+++ tests/PixTestBBMap.cc
@@ -20,7 +20,7 @@ using namespace pxar;
 ClassImp(PixTestBBMap)
 
 //------------------------------------------------------------------------------
-PixTestBBMap::PixTestBBMap(PixSetup *a, std::string name): PixTest(a, name), 
+PixTestBBMap::PixTestBBMap(PixSetup *a, std::string name): PixTest(a, name),
   fParNtrig(0), fParVcalS(200), fDumpAll(-1), fDumpProblematic(-1) {
   PixTest::init();
   init();
@@ -44,28 +44,28 @@ bool PixTestBBMap::setParameter(string parName, string sval) {
       sval.erase(remove(sval.begin(), sval.end(), ' '), sval.end());
 
       stringstream s(sval);
-      if (!parName.compare( "ntrig")) { 
-	s >> fParNtrig; 
+      if (!parName.compare( "ntrig")) {
+	s >> fParNtrig;
 	setToolTips();
 	return true;
       }
-      if (!parName.compare( "vcals")) { 
-	s >> fParVcalS; 
+      if (!parName.compare( "vcals")) {
+	s >> fParVcalS;
 	setToolTips();
 	return true;
       }
 
       if (!parName.compare("dumpall")) {
-	PixUtil::replaceAll(sval, "checkbox(", ""); 
-	PixUtil::replaceAll(sval, ")", ""); 
-	fDumpAll = atoi(sval.c_str()); 
+	PixUtil::replaceAll(sval, "checkbox(", "");
+	PixUtil::replaceAll(sval, ")", "");
+	fDumpAll = atoi(sval.c_str());
 	setToolTips();
       }
 
       if (!parName.compare("dumpallproblematic")) {
-	PixUtil::replaceAll(sval, "checkbox(", ""); 
-	PixUtil::replaceAll(sval, ")", ""); 
-	fDumpProblematic = atoi(sval.c_str()); 
+	PixUtil::replaceAll(sval, "checkbox(", "");
+	PixUtil::replaceAll(sval, ")", "");
+	fDumpProblematic = atoi(sval.c_str());
 	setToolTips();
       }
 
@@ -76,7 +76,7 @@ bool PixTestBBMap::setParameter(string parName, string sval) {
 
 // ----------------------------------------------------------------------
 void PixTestBBMap::resetDirectory() {
-  fDirectory = gFile->GetDirectory("BumpBonding"); 
+  fDirectory = gFile->GetDirectory("BumpBonding");
 }
 
 
@@ -84,7 +84,7 @@ void PixTestBBMap::resetDirectory() {
 void PixTestBBMap::init() {
   LOG(logDEBUG) << "PixTestBBMap::init()";
   // -- NOTE: The hard-coded name is really bad. This should be fName.
-  //    not going to change in production, as this affects moreweb. 
+  //    not going to change in production, as this affects moreweb.
   fDirectory = gFile->GetDirectory("BumpBonding");
   if (!fDirectory) {
     fDirectory = gFile->mkdir("BumpBonding");
@@ -101,25 +101,25 @@ void PixTestBBMap::setToolTips() {
 
 // // ----------------------------------------------------------------------
 // void PixTestBBMap::writeOutput() {
-//   std::list<TH1*>::iterator il; 
+//   std::list<TH1*>::iterator il;
 //   string name("BumpBonding");
 //   cout << "name = " << name << " fDirectory = " << fDirectory << endl;
-//   fDirectory->cd(); 
+//   fDirectory->cd();
 //   for (il = fHistList.begin(); il != fHistList.end(); ++il) {
-//     (*il)->SetDirectory(fDirectory); 
-//     (*il)->Write(); 
+//     (*il)->SetDirectory(fDirectory);
+//     (*il)->Write();
 //   }
 //   clearHistList();
 
-//   TH1D *h = (TH1D*)gDirectory->Get("ha"); 
+//   TH1D *h = (TH1D*)gDirectory->Get("ha");
 //   if (h) {
-//     h->SetDirectory(fDirectory); 
+//     h->SetDirectory(fDirectory);
 //     h->Write();
 //   }
 
-//   h = (TH1D*)gDirectory->Get("hd"); 
+//   h = (TH1D*)gDirectory->Get("hd");
 //   if (h) {
-//     h->SetDirectory(fDirectory); 
+//     h->SetDirectory(fDirectory);
 //     h->Write();
 //   }
 
@@ -140,7 +140,7 @@ void PixTestBBMap::doTest() {
   cacheDacs();
   PixTest::update();
   bigBanner(Form("PixTestBBMap::doTest() Ntrig = %d, VcalS = %d (high range)", fParNtrig, fParVcalS));
- 
+
   fDirectory->cd();
 
   fApi->_dut->testAllPixels(true);
@@ -149,52 +149,52 @@ void PixTestBBMap::doTest() {
 
   int flag(FLAG_CALS);
   fApi->setVcalHighRange();
-  fApi->setDAC("vcal", fParVcalS);    
+  fApi->setDAC("vcal", fParVcalS);
 
   int result(1);
   if (fDumpAll) result |= 0x20;
   if (fDumpProblematic) result |= 0x10;
 
-  fNDaqErrors = 0; 
+  fNDaqErrors = 0;
   vector<TH1*>  thrmapsCals = scurveMaps("VthrComp", "calSMap", fParNtrig, 0, 149, -1, -1, result, 1, flag);
 
   // -- relabel negative thresholds as 255 and create distribution list
-  vector<TH1D*> dlist; 
+  vector<TH1D*> dlist;
   TH1D *h(0);
   for (unsigned int i = 0; i < thrmapsCals.size(); ++i) {
     for (int ix = 0; ix < thrmapsCals[i]->GetNbinsX(); ++ix) {
       for (int iy = 0; iy < thrmapsCals[i]->GetNbinsY(); ++iy) {
-	if (thrmapsCals[i]->GetBinContent(ix+1, iy+1) < 0) thrmapsCals[i]->SetBinContent(ix+1, iy+1, 255.);
+	if (thrmapsCals[i]->GetBinContent(ix+1, iy+1) < 1.0) thrmapsCals[i]->SetBinContent(ix+1, iy+1, 255.);
       }
     }
     h = distribution((TH2D*)thrmapsCals[i], 256, 0., 256.);
 
-    dlist.push_back(h); 
-    fHistList.push_back(h); 
+    dlist.push_back(h);
+    fHistList.push_back(h);
   }
 
   restoreDacs();
 
   // -- summary printout
-  string bbString(""), bbCuts(""); 
-  int bbprob(0); 
-  int nPeaks(0), cutDead(0); 
-  TSpectrum s; 
+  string bbString(""), bbCuts("");
+  int bbprob(0);
+  int nPeaks(0), cutDead(0);
+  TSpectrum s;
   for (unsigned int i = 0; i < dlist.size(); ++i) {
     h = (TH1D*)dlist[i];
-    nPeaks = s.Search(h, 5, "", 0.01); 
+    nPeaks = s.Search(h, 5, "", 0.01);
     LOG(logDEBUG) << "found " << nPeaks << " peaks in " << h->GetName();
-    cutDead = fitPeaks(h, s, nPeaks); 
+    cutDead = fitPeaks(h, s, nPeaks);
 
     bbprob = static_cast<int>(h->Integral(cutDead, h->FindBin(255)));
-    bbString += Form(" %4d", bbprob); 
-    bbCuts   += Form(" %4d", cutDead); 
+    bbString += Form(" %4d", bbprob);
+    bbCuts   += Form(" %4d", cutDead);
 
-    TArrow *pa = new TArrow(cutDead, 0.5*h->GetMaximum(), cutDead, 0., 0.06, "|>"); 
+    TArrow *pa = new TArrow(cutDead, 0.5*h->GetMaximum(), cutDead, 0., 0.06, "|>");
     pa->SetArrowSize(0.1);
     pa->SetAngle(40);
     pa->SetLineWidth(2);
-    h->GetListOfFunctions()->Add(pa); 
+    h->GetListOfFunctions()->Add(pa);
 
   }
 
@@ -202,11 +202,11 @@ void PixTestBBMap::doTest() {
     h->Draw();
     fDisplayedHist = find(fHistList.begin(), fHistList.end(), h);
   }
-  PixTest::update(); 
-  
+  PixTest::update();
+
   int seconds = t.RealTime();
   LOG(logINFO) << "PixTestBBMap::doTest() done"
-	       << (fNDaqErrors>0? Form(" with %d decoding errors: ", static_cast<int>(fNDaqErrors)):"") 
+	       << (fNDaqErrors>0? Form(" with %d decoding errors: ", static_cast<int>(fNDaqErrors)):"")
 	       << ", duration: " << seconds << " seconds";
   LOG(logINFO) << "number of dead bumps (per ROC): " << bbString;
   LOG(logINFO) << "separation cut       (per ROC): " << bbCuts;
@@ -225,11 +225,11 @@ int PixTestBBMap::fitPeaks(TH1D *h, TSpectrum &s, int npeaks) {
   Float_t *xpeaks = s.GetPositionX();
 #endif
 
-  string name; 
+  string name;
   double lcuts[3]; lcuts[0] = lcuts[1] = lcuts[2] = 255.;
-  TF1 *f(0); 
+  TF1 *f(0);
   double peak, sigma;
-  int fittedPeaks(0); 
+  int fittedPeaks(0);
   for (int p = 0; p < npeaks; ++p) {
     double xp = xpeaks[p];
     if (p > 1) continue;
@@ -239,60 +239,60 @@ int PixTestBBMap::fitPeaks(TH1D *h, TSpectrum &s, int npeaks) {
     if (xp < 15) {
       continue;
     }
-    name = Form("gauss_%d", p); 
+    name = Form("gauss_%d", p);
     f = new TF1(name.c_str(), "gaus(0)", 0., 256.);
     int bin = h->GetXaxis()->FindBin(xp);
     double yp = h->GetBinContent(bin);
     f->SetParameters(yp, xp, 2.);
-    h->Fit(f, "Q+"); 
+    h->Fit(f, "Q+");
     ++fittedPeaks;
-    peak = h->GetFunction(name.c_str())->GetParameter(1); 
-    sigma = h->GetFunction(name.c_str())->GetParameter(2); 
+    peak = h->GetFunction(name.c_str())->GetParameter(1);
+    sigma = h->GetFunction(name.c_str())->GetParameter(2);
     if (0 == p) {
-      lcuts[0] = peak + 3*sigma; 
+      lcuts[0] = peak + 3*sigma;
       if (h->Integral(h->FindBin(peak + 10.*sigma), 250) > 10.) {
 	lcuts[1] = peak + 5*sigma;
       } else {
 	lcuts[1] = peak + 10*sigma;
       }
     } else {
-      lcuts[1] = peak - 3*sigma; 
-      lcuts[2] = peak - sigma; 
+      lcuts[1] = peak - 3*sigma;
+      lcuts[2] = peak - sigma;
     }
     delete f;
   }
-  
-  int startbin = (int)(0.5*(lcuts[0] + lcuts[1])); 
-  int endbin = (int)(lcuts[1]); 
+
+  int startbin = (int)(0.5*(lcuts[0] + lcuts[1]));
+  int endbin = (int)(lcuts[1]);
   if (endbin <= startbin) {
-    endbin = (int)(lcuts[2]); 
+    endbin = (int)(lcuts[2]);
     if (endbin < startbin) {
       endbin = 255.;
     }
   }
 
-  int minbin(0); 
-  double minval(999.); 
-  
+  int minbin(0);
+  double minval(999.);
+
   for (int i = startbin; i <= endbin; ++i) {
     if (h->GetBinContent(i) < minval) {
       if (1 == fittedPeaks) {
 	if (0 == h->Integral(i, i+4)) {
-	  minval = h->GetBinContent(i); 
-	  minbin = i; 
+	  minval = h->GetBinContent(i);
+	  minbin = i;
 	} else {
 	  minbin = endbin;
 	}
       } else {
-	minval = h->GetBinContent(i); 
-	minbin = i; 
+	minval = h->GetBinContent(i);
+	minbin = i;
       }
     }
   }
-  
-  LOG(logDEBUG) << "cut for dead bump bonds: " << minbin << " (obtained for minval = " << minval << ")" 
-		<< " start: " << startbin << " .. " << endbin 
+
+  LOG(logDEBUG) << "cut for dead bump bonds: " << minbin << " (obtained for minval = " << minval << ")"
+		<< " start: " << startbin << " .. " << endbin
 		<< " last peak: " << peak << " last sigma: " << sigma
 		<< " lcuts[0] = " << lcuts[0] << " lcuts[1] = " << lcuts[1];
-  return minbin+1; 
+  return minbin+1;
 }
