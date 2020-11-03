df09c2cc9827e88ebc9b5b4f41da8fe62c0956b8
Urs Langenegger <urslangenegger@gmail.com>
2020-11-03 08:24:48 +0100

    add printout

--- ana/test0.C
+++ ana/test0.C
@@ -51,6 +51,7 @@ void test0(string filename, string mod = "") {
       for (int ix = 1; ix < 53; ++ix) {
 	for (int iy = 1; iy < 81; ++iy) {
 	  if (h2->GetBinContent(ix, iy) < 1) {
+	    cout << "missing BB in ROC " << i << ": " << ix << "/" << iy << endl;
 	    hall->Fill(ix, iy);
 	  }
 	}
